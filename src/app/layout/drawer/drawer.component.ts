87e6fbf5acb3b0cb586ff099bc08abd665ef9dd6
Nikola Jeremic <nikola87kg@gmail.com>
2019-04-07 16:50:47 +0200

    drawer fixed position

--- src/app/layout/drawer/drawer.component.ts
+++ src/app/layout/drawer/drawer.component.ts
@@ -1,13 +1,17 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectorRef,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-drawer',
   templateUrl: './drawer.component.html',
   styleUrls: ['./drawer.component.scss']
 })
-export class DrawerComponent implements OnDestroy {
-
+export class DrawerComponent implements OnInit, OnDestroy {
   mobileQuery: MediaQueryList;
   smallScreen = '(max-width: 600px)';
   isLoggedOut: boolean;
@@ -22,8 +26,10 @@ export class DrawerComponent implements OnDestroy {
     this.mobileQuery.addListener(() => changeDetectorRef.detectChanges());
   }
 
+  ngOnInit() {
+  }
+
   ngOnDestroy() {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-
 }
